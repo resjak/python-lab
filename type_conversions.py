654e5c4142e0c5b0aa11bd25e96c9186b089bb7a
resjak <resler_j@outlook.com>
2024-03-19 15:06:32 +0100

    Added new scripts

---
+++ type_conversions.py
@@ -0,0 +1,9 @@
+float_value = 3.14
+int_value = int(float_value)
+print(f"Converted float is: {int_value}")
+
+
+
+int_number = 5
+float_number = float(int_number)
+print(f"Converted integer is: {float_number}")
